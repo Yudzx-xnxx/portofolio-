4523253919a763642bb56c8f359e72486433a38e
Yudzx-xnxx <yudzsenpai@gmail.com>
2025-02-20 18:05:48 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuBtn = document.getElementById("menu-btn");
+    const menu = document.getElementById("menu");
+
+    menuBtn.addEventListener("click", function() {
+        menu.classList.toggle("hidden");
+    });
+});
