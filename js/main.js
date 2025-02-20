7b882cb9d4795926e3099d5021a1a55a1fc6f426
Yudzx-xnxx <yudzsenpai@gmail.com>
2025-02-20 18:08:36 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuBtn = document.getElementById("menu-btn");
-    const menu = document.getElementById("menu");
-
-    menuBtn.addEventListener("click", function() {
-        menu.classList.toggle("hidden");
-    });
-});
