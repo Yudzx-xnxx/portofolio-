e096ee1b94dd28fac9b39b137096368ab258dc59
Yudzx-xnxx <yudzsenpai@gmail.com>
2025-02-21 06:15:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    document.getElementById('menu').classList.toggle('hidden');
+document.addEventListener("DOMContentLoaded", function () {
+    // Toggle Menu Titik Tiga
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.style.display = navMenu.style.display === "block" ? "none" : "block";
+    });
 });
